dd5da9ccfc779f2ddcf940546009c40cc43f7fbd
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-19 03:05:17 +0400

    Added Task 6

---
+++ Task6/BiggestLetterCount/Program.cs
@@ -0,0 +1,21 @@
+﻿﻿string word = "kertenkele";
+int max = 0;
+char l = ' ';
+foreach(char letter in word){
+    int count = 0;
+   
+    for(int i = 0; i < word.Length; i++)
+        {
+            if(letter == word[i])
+                {
+                count++;
+                }
+        }
+        if(count > max)
+            {
+                max = count;
+                l = letter;                
+            }
+    }
+    Console.WriteLine(max);
+    Console.WriteLine(l);
