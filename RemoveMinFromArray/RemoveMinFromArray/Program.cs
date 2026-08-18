f149df036f56fc5f4145edde70aa03ec8bd40f98
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-18 23:29:41 +0400

    Added Task 2

---
+++ RemoveMinFromArray/RemoveMinFromArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+int[] numbers =  {7, 12, 5, 8};
+int min = numbers[0];
+for(int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i]< min)
+    {
+        min = numbers[i];
+    }
+}
+foreach(int number in numbers)
+{
+    if(number == min)
+    {
+        continue;
+    }
+    Console.WriteLine(number);
+}
