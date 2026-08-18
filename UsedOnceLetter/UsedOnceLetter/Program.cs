d59865d786670cb9dff04717a142a888f21d933e
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-19 00:23:44 +0400

    AddTask 3

---
+++ UsedOnceLetter/UsedOnceLetter/Program.cs
@@ -0,0 +1,17 @@
+﻿string word = "kertenkele";
+foreach(char letter in word){
+    int count = 0;
+    for(int i = 0; i < word.Length; i++)
+        {
+            if(letter == word[i])
+                {
+                count++;
+                }
+        }
+        if(count == 1)
+            {
+                Console.WriteLine(letter);
+                break;
+            }
+    }
+    
