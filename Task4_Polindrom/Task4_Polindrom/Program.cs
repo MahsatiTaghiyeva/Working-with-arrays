424e914ce48aa34897bf0f1609f97374230be129
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-19 02:45:08 +0400

    Added task 4

---
+++ Task4_Polindrom/Task4_Polindrom/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = 121;
+int m = 2553;
+for(int i = n; i< m; i++)
+{
+    int num = i; //121
+
+    int reversed = 0 ;
+    while (num > 0)
+    {
+        int digit = num % 10;  //1 2 1
+        reversed = reversed*10 + digit; //1 12 121
+        num/= 10; //12 1 0
+
+    }
+    if(reversed == i)
+    {
+        Console.WriteLine(i);
+    }
+}
