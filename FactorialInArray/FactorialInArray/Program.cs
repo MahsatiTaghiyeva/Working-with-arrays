cdc6467189713e5549696f1957bc600eda7bbc10
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-18 23:14:19 +0400

    Added Task 1

---
+++ FactorialInArray/FactorialInArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numbers = {5, 4, 6};
+for( int i = 0; i < numbers.Length; i++)
+{
+    int factorial = 1;
+    for(int j = 1; j <= numbers[i]; j++)
+    {
+        factorial *= j;
+    }
+    numbers[i] = factorial;
+}
+foreach(int number in numbers)
+{
+    Console.WriteLine(number);
+}
