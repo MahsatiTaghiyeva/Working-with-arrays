de2e928d39d38374837d182a597e670b3fb28868
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-19 16:22:18 +0400

    Added new array to store the result

--- Task2/RemoveMinFromArray/Program.cs
+++ Task2/RemoveMinFromArray/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Globalization;
 
 int[] numbers =  {7, 12, 5, 8};
+int[] newnumbers = new int[4];
 int min = numbers[0];
 for(int i = 0; i < numbers.Length; i++)
 {
@@ -9,11 +10,15 @@ for(int i = 0; i < numbers.Length; i++)
         min = numbers[i];
     }
 }
+int index = 0;
 foreach(int number in numbers)
 {
+    
     if(number == min)
     {
         continue;
     }
+    newnumbers[index] = number;
+    index++;
     Console.WriteLine(number);
 }
