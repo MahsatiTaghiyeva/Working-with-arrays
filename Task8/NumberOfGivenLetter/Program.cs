b1ab7a6b54d2f6cdefa6c9a00c9b0e8d9cb584bd
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-19 14:01:44 +0400

    added Task 8

---
+++ Task8/NumberOfGivenLetter/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] list = {"apple", "banana", "watermelon", "orange"};
+char find = 'a';
+int count = 0;
+foreach(string word in list)
+{
+    for(int i =0; i < word.Length; i++)
+    {
+        if(word[i] == find)
+        {
+            count ++;
+        }
+    }
+}
+Console.WriteLine(count);
