2aa7f1fda63ac4056106b5389ae1802bed695a5f
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-19 15:26:55 +0400

    added task 9

---
+++ Task9/DeleteFirstSpace/Program.cs
@@ -0,0 +1,18 @@
+﻿string sentence = "   salam necesen?   ";
+string newSentence = "";
+bool check = false;
+for(int i = 0; i < sentence.Length; i++)
+{
+    if(sentence[i] != ' ')
+    {
+        check = true;
+    }
+    if (check)
+    {
+        newSentence += sentence[i];
+    }
+    
+    
+}
+
+Console.WriteLine(newSentence);
