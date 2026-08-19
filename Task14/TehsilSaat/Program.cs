add956ad4108f367f0b1e716ee9a8a4ff594fb60
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-19 16:56:06 +0400

    added task 14

---
+++ Task14/TehsilSaat/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Tehsil novunu secin: ");
+string tehsil = Convert.ToString(Console.ReadLine());
+switch (tehsil)
+{
+    case "programming":
+        Console.WriteLine("400 saat");
+        break;
+    case "design":
+        Console.WriteLine("250 saat");
+        break;
+    case "system":
+        Console.WriteLine("200 saat");
+        break;
+    default:
+        Console.WriteLine("təhsil novu yanlisdir");
+        break;
+
+}
