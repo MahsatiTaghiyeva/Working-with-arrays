b6c705eb0372bf1b343f5caa7cc423dbfdc90e97
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-19 15:54:37 +0400

    added task 10

---
+++ Task10/Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿int num1 = 34;
+int num2 =23;
+Console.WriteLine("Simvolu daxil et: ");
+char operation = Convert.ToChar(Console.ReadLine());
+while(operation != '+' && operation != '-' && operation != '*' && operation != '/'){
+Console.WriteLine("Enter Valid sign! Simvolu daxil et: ");
+operation = Convert.ToChar(Console.ReadLine());}
+switch (operation)
+{
+    case '+':
+        Console.WriteLine(num1+num2);
+        break;
+    case '-':
+        Console.WriteLine(num1-num2);
+        break;
+    case '*':
+        Console.WriteLine(num1*num2);
+        break;
+    case '/':
+        Console.WriteLine(num1/num2);
+        break;
+}
