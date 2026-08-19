b666c90d6268f39e3c51137a590db48926f422fb
MahsatiTaghiyeva <mahsatitaghiyeva@gmail.com>
2026-08-19 16:46:11 +0400

    addae task 13

---
+++ Task13/SquareCalc/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Musbet ve cut reqem daxil et: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while(n%2!=0 || n < 0)
+{
+    Console.WriteLine("Musbet ve cut reqem daxil et: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+n = n*n;
+Console.WriteLine(n);
